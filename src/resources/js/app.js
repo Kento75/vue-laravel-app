a3e8d5ffb855c185a9e60feb106e3a390c3ccff9
Kento75 <kento2github@gmail.com>
2019-02-23 07:26:26 +0900

    Add Vue Store and axios config

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
 import router from "./router";
+import store from "./store";
+import "./bootstrap";
 
 import App from "./App.vue";
 
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App />'
 })
