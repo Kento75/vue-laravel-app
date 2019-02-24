508b952fce90469ee336a6351bef21aef709abd1
Kento75 <kento2github@gmail.com>
2019-02-24 19:59:48 +0900

    Add regist route and current User check logic

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -5,10 +5,17 @@ import "./bootstrap";
 
 import App from "./App.vue";
 
-new Vue({
-  el: '#app',
-  router,
-  store,
-  components: { App },
-  template: '<App />'
-})
+// Vueインスタンス生成前に現在のユーザーを取得
+const createApp = async() => {
+  await store.dispatch("auth/currentUser")
+
+  new Vue({
+    el: '#app',
+    router,
+    store,
+    components: { App },
+    template: '<App />'
+  })
+};
+
+createApp();
