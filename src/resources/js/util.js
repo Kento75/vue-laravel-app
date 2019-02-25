184ae7835a85d0df0b7679e3c6db108fb0f628cb
Kento75 <kento2github@gmail.com>
2019-02-26 06:26:45 +0900

    Add error hadling

--- src/resources/js/util.js
+++ src/resources/js/util.js
@@ -1,3 +1,8 @@
+export const OK = 200;
+export const CREATED = 201;
+export const INTERNAL_SERVER_ERROR = 500;
+export const UNPROCESSABLE_ENTITY = 422;  // バリデーションエラー(Laravel)
+
 /**
  * クッキーの値を取得
  * @param {String} searchKey 検索キー
