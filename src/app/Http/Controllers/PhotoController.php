eb5709d0dbdf7a35b2e939974950f130a474ab1e
Kento75 <kento2github@gmail.com>
2019-03-12 06:14:16 +0900

    Add PhotoComment API

--- src/app/Http/Controllers/PhotoController.php
+++ src/app/Http/Controllers/PhotoController.php
@@ -1,23 +1,42 @@
 <?php
-
 namespace App\Http\Controllers;
-
+use App\Comment;
+use App\Http\Requests\StoreComment;
 use App\Http\Requests\StorePhoto;
 use App\Photo;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
-
+use phpDocumentor\Reflection\Types\Integer;
 class PhotoController extends Controller
 {
     public function __construct()
     {
         // 認証が必要
-        //　写真一覧画面、ダウンロード機能は認証を必要としない
-        $this->middleware('auth')->except(['index', 'show', 'download']);
+        $this->middleware('auth')->except(['index', 'download', 'show']);
     }
-
+    /**
+     * 写真一覧
+     * @return \Illuminate\Contracts\Pagination\LengthAwarePaginator
+     */
+    public function index()
+    {
+        $photos = Photo::with(['owner'])
+            ->orderBy(Photo::CREATED_AT, 'desc')->paginate();
+        return $photos;
+    }
+    /**
+     * 写真詳細
+     * @param string $id
+     * @return Photo
+     */
+    public function show(string $id)
+    {
+        $photo = Photo::where('id', $id)
+            ->with(['owner', 'comments.author'])->first();
+        return $photo ?? abort(404);
+    }
     /**
      * 写真投稿
      * @param StorePhoto $request
@@ -25,61 +44,37 @@ class PhotoController extends Controller
      */
     public function create(StorePhoto $request)
     {
-        // 投稿写真の拡張子を取得する
         $extension = $request->photo->extension();
-
         $photo = new Photo();
-
-        // インスタンス生成時に割り振られたランダムなID値と
-        // 本来の拡張子を組み合わせてファイル名とする
         $photo->filename = $photo->id . '.' . $extension;
-
-        // S3にファイルを保存する
-        // 第三引数の'public'はファイルを公開状態で保存するため
         Storage::cloud()->putFileAs('', $request->photo, $photo->filename, 'public');
-
-        // データベースエラー時にファイル削除を行うため
-        // トランザクションを利用する
         DB::beginTransaction();
-
         try {
             Auth::user()->photos()->save($photo);
             DB::commit();
         } catch (\Exception $exception) {
             DB::rollBack();
-            // DBとの不整合を避けるためアップロードしたファイルを削除
             Storage::cloud()->delete($photo->filename);
             throw $exception;
         }
-
-        // リソースの新規作成なので
-        // レスポンスコードは201(CREATED)を返却する
         return response($photo, 201);
     }
-
     /**
-     * 写真一覧
+     * コメント投稿
+     * @param Photo $photo
+     * @param StoreComment $request
+     * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function addComment(Photo $photo, StoreComment $request)
     {
-        $photos = Photo::with(["owner"])
-            ->orderBy(Photo::CREATED_AT, "desc")->paginate();
-
-        return $photos;
+        $comment = new Comment();
+        $comment->content = $request->get('content');
+        $comment->user_id = Auth::user()->id;
+        $photo->comments()->save($comment);
+        $new_comment = Comment::where('id', $comment->id)->with('author')->first();
+        return response($new_comment, 201);
     }
-
-    /**
-     * 写真詳細
-     * @param string $id
-     * @return Photo
-     */
-    public function show(string $id) {
-        $photo = Photo::where("id", $id)->with(["owner"])->first();
-
-        return $photo ?? abort(404);
-    }
-
-    /**
+    /*
      * 写真ダウンロード
      * @param Photo $photo
      * @return \Illuminate\Http\Response
@@ -90,12 +85,10 @@ class PhotoController extends Controller
         if (! Storage::cloud()->exists($photo->filename)) {
             abort(404);
         }
-
         $headers = [
             'Content-Type' => 'application/octet-stream',
             'Content-Disposition' => 'attachment; filename="' . $photo->filename . '"',
         ];
-
         return response(Storage::cloud()->get($photo->filename), 200, $headers);
     }
 }
--- src/tests/Feature/PhotoDetailApiTest.php
+++ src/tests/Feature/PhotoDetailApiTest.php
@@ -1,47 +1,40 @@
 <?php
-
 namespace Tests\Feature;
-
-use App\Photo;
 use App\Comment;
-
+use App\Photo;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-
 class PhotoDetailApiTest extends TestCase
 {
     use RefreshDatabase;
-
     /**
      * @test
      */
-    public function should_正しい構造のJSONを返す()
+    public function should_正しい構造のJSONを返却する()
     {
-        factory(Photo::class)->create()->each(function($photo) {
+        factory(Photo::class)->create()->each(function ($photo) {
             $photo->comments()->saveMany(factory(Comment::class, 3)->make());
-        })
+        });
         $photo = Photo::first();
-
-        $response = $this->json("GET", route("photo.show", [
-            "id" => $photo->id,
+        $response = $this->json('GET', route('photo.show', [
+            'id' => $photo->id,
         ]));
-
         $response->assertStatus(200)
             ->assertJsonFragment([
-                "id" => $photo->id,
-                "url" => $photo->url,
-                "owner" => [
-                    "name" => $photo->owner->name,
+                'id' => $photo->id,
+                'url' => $photo->url,
+                'owner' => [
+                    'name' => $photo->owner->name,
                 ],
-                "comments" => $photo->comments
-                    ->sortByDesc("id")
-                    ->map(function($commen) {
+                'comments' => $photo->comments
+                    ->sortByDesc('id')
+                    ->map(function ($comment) {
                         return [
-                            "author" => [
-                                "name" => $comment->author->name,
+                            'author' => [
+                                'name' => $comment->author->name,
                             ],
-                            "content" => $comment->content,
+                            'content' => $comment->content,
                         ];
                     })
                     ->all(),
